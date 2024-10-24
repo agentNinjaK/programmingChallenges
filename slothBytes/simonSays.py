f27219ff9272517ef5d6c3cfb66bd79fae2f575f
agentNinjaK <2199055+agentNinjaK@users.noreply.github.com>
2024-10-24 12:23:05 -0500

    changing up the prints

--- slothBytes/simonSays.py
+++ slothBytes/simonSays.py
@@ -4,7 +4,6 @@ def simon_says(args):
     answer = 0
     for val in args:
         if val.startswith("Simon says"):
-            print(val)
             number = int(re.search(r'\d+', val).group())
             if("add") in val:
                 answer += number
@@ -12,26 +11,27 @@ def simon_says(args):
                 answer -= number
             elif("multiply") in val:
                 answer *= number
-    print(answer)
     return answer
 
 #examples (not tests!)
-simon_says([
+print(simon_says([
   "Simon says add 4",
   "Simon says add 6",
   "Then add 5"
-])
+]))
 #output = 10
 
-simon_says([
+print(simon_says([
   "Susan says add 10",
   "Simon says add 3",
   "Simon says multiply by 8"
-])
+]))
 #output = 24
 
-simon_says([
+print(simon_says([
   "Firstly, add 4",
   "Simeon says subtract 100" # Look at the name closely :)
-])
+]))
 #output = 0
+
+#TODO actual tests
